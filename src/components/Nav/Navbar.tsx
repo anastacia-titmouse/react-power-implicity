7b56f38f2716081826a65fe58355bc23379aee59
Anastacia Protasenya <anastacia.sinitsa@gmail.com>
2022-10-28 21:48:40 +0300

    fix links

--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -6,17 +6,17 @@ import "./navbar.scss";
 export const Navbar = () => {
   return (
     <div className="navbar">
-      <a className="navbar__link--logo" href=" ">
+      <a className="navbar__link--logo" href="#">
         <LogoIcon className="navbar__logo" />
       </a>
       <nav className="navbar__link--wrapper">
-        <a className="navbar__link" href=" ">
+        <a className="navbar__link" href="#">
           Features
         </a>
-        <a className="navbar__link" href=" ">
+        <a className="navbar__link" href="#">
           Partners
         </a>
-        <a className="navbar__link" href=" ">
+        <a className="navbar__link" href="#">
           Stories
         </a>
       </nav>
